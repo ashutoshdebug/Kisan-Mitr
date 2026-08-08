8e5de3b71ee5ce6b49121bc8fde1c2285e60e73d
Ashutosh Kumar Tiwari <ashutoshkumart82@gmail.com>
2026-08-08 21:52:19 +0530

    Routing and login page initial ver

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+from flask_livereload import LiveReload
+
+app = Flask(__name__)
+
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+# Do not use this in production
+livereload = LiveReload(app)
+
+@app.route("/")
+def landingPage():
+    return render_template('index.html')
+
+@app.route("/login", methods = ["GET", "POST"])
+def login_page():
+    if request.method == "POST":
+        pass
+    return render_template('login.html')
